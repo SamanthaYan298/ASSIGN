142618237526aff71b03fa602446fed61fbef26f
Yisha Yan <yisha-y@hotmail.com>
2023-09-17 14:28:55 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// declaring then assigning - use camelCasing as needed
+
+/** Rules 
+ * Variables you create do not have quotes around time. Only sentence have quotations,
+*/
+
+/**Example 1 
+ * Declare the variable cat
+ * On a new line then assign cat to the number/integer/whole number 1,
+*/
+var cat; // declare
+cat = 1; // assigning
+
+
+/** Example 2 
+ * Declare the variable light bulbs
+ * On a new line assign light bulbs to 4
+*/
+var lightBulbs; // declare
+lightbulbs = 4; // assign
+console.log(lightBulbs)
+
+/** Example 3 
+ * Declare the variable brand
+ * On a new line assign brand to Google
+*/
+
+var brand;
+brand = "Google";
+console.log(brand)
+
+/** Example 4 
+ * Declare the variable job title
+ * On a new line then assign job title to website Developer
+*/
+var jobtitle;
+jobTitle = 'Website Developer';
+console.log(jobTitle)
---
+++ test.js
@@ -0,0 +1,16 @@
+var number = 1
+console.log(number);
+number = 3;
+console.log(number);
+
+
+var x =10
+var y = 5
+var z = x + y
+console.log(z)
+
+/** @desc finding an item
+ * @param {*} item - an item that the person is looking for
+ */
+function find(item) { console.log (item);}
+find ("book");
